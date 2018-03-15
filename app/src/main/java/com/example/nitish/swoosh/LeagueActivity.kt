08a8f08b9de0e53b3550bfd0e0ea955b1a842250
Nitish Rana <nitish@maropost.com>
2018-03-15 14:24:45 +0530

    Added skill screen

--- app/src/main/java/com/example/nitish/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/nitish/swoosh/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.example.nitish.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        leagueNextButton.setOnClickListener {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            startActivity(skillIntent)
+        }
+
     }
 }
