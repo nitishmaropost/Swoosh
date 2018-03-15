24779fd9aea8d6761cc4bbe16eae6083af581b37
Nitish Rana <nitish@maropost.com>
2018-03-15 16:18:41 +0530

    Added final screen

---
+++ app/src/main/java/com/example/nitish/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nitish.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+    }
+}
