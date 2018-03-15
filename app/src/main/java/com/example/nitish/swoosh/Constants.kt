a30077998264a8665215c55f5f188ff7d5d93ad8
Nitish Rana <nitish@maropost.com>
2018-03-15 16:02:42 +0530

    passed value from league screen to skill screen. Also handled toggle buttons on league screen.

---
+++ app/src/main/java/com/example/nitish/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.nitish.swoosh
+
+/**
+ * Created by Nitish on 15/03/18.
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/nitish/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/nitish/swoosh/LeagueActivity.kt
@@ -1,20 +1,58 @@
 package com.example.nitish.swoosh
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
         leagueNextButton.setOnClickListener {
-            val skillIntent = Intent(this, SkillActivity::class.java)
-            startActivity(skillIntent)
+            if (!selectedLeague.isNullOrEmpty()) {
+                val skillIntent = Intent(this, SkillActivity::class.java)
+                skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+                startActivity(skillIntent)
+            }
+            else
+            {
+                Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+            }
         }
 
+        mensLeagueBtn.setOnClickListener {
+            if (mensLeagueBtn.isChecked)
+            {
+                selectedLeague = "Mens"
+            }
+
+            womensLeagueBtn.isChecked = false
+            coedLeagueBtn.isChecked = false
+        }
+
+        womensLeagueBtn.setOnClickListener {
+            if (mensLeagueBtn.isChecked)
+            {
+                selectedLeague = "Womens"
+            }
+
+            mensLeagueBtn.isChecked = false
+            coedLeagueBtn.isChecked = false
+        }
+
+        coedLeagueBtn.setOnClickListener {
+            if (mensLeagueBtn.isChecked)
+            {
+                selectedLeague = "Co-ed"
+            }
+
+            mensLeagueBtn.isChecked = false
+            womensLeagueBtn.isChecked = false
+        }
     }
 }
